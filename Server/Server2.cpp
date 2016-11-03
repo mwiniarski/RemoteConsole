d3f969790083c92e2a4dc528833f7fd14a0469bc
Michal Winiarski <mw.pari@gmail.com>
2016-11-03 11:40:55 +0100

    finally working tcp server

--- Server/Server2.cpp
+++ Server/Server2.cpp
@@ -3,83 +3,67 @@
 #include <iostream>
 #include <ctime>
 #include <stdlib.h> //atoi
-
+#include <unistd.h>
+#include <errno.h>
 using namespace std;
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <string.h>
 
 #define WINDOWS 5521859
 #define CLIENT 3263094
 
 int main(int argc, char* argv[]) {
+
     int PORT = 8888;
     if(argc == 2) {
 		PORT = atoi(argv[1]);
     }
+    struct sockaddr_in server_addr, client_addr;
+    int sock_server, sock_new;
 
-	struct sockaddr_in si_server, si_windows, si_client, si_unknown;
+    unsigned appInfo;
+    char buf[128];
+    socklen_t slen = sizeof(sockaddr_in);
 
-
-	int server_socket;
-	unsigned appInfo, buf;
-	socklen_t slen = sizeof(sockaddr_in), recv_len;
-
-    //create a UDP socket
-   if ((server_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
+    //create a TCP socket
+   if ((sock_server = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 	cout << "Socket";
 	return 0;
    }
 
     // zero out the structure
-    memset(&si_server, 0, sizeof(si_server));
-    memset(&si_windows, 0, sizeof(si_windows));
-    memset(&si_client, 0, sizeof(si_client));
-    memset(&si_unknown, 0, sizeof(si_unknown));
+    bzero((char *)&server_addr, sizeof(server_addr));
 
-    //set port && any allow any incoming address
-    si_server.sin_family = AF_INET;
-    si_server.sin_port = htons(PORT);
-    si_server.sin_addr.s_addr = htonl(INADDR_ANY);
+    //set port && allow any incoming address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
     //bind socket
-    if( bind(server_socket , (struct sockaddr*)&si_server, sizeof(si_server) ) == -1) {
+    if( bind(sock_server , (struct sockaddr *) &server_addr, sizeof(server_addr) ) < 0) {
 
 		cout << "Bind";
 		return 0;
     }
 
-    while (true) {
-	//try to receive some data, this is a blocking call
-	recv_len = recvfrom(server_socket, &buf, sizeof(buf), 0, (struct sockaddr *) &si_unknown, &slen);
-
-	//interpret data
-	if (buf == CLIENT) {
-		si_client.sin_addr = si_unknown.sin_addr;
-		si_client.sin_port = si_unknown.sin_port;
-		cout << "CLIENT endpoint: " << inet_ntoa(si_client.sin_addr) << ntohs(si_client.sin_port) << endl; }
-	else if (buf == WINDOWS) {
-		si_windows.sin_addr = si_unknown.sin_addr;
-		si_windows.sin_port = si_unknown.sin_port;
-		cout << "WINDOWS endpoint: " << inet_ntoa(si_client.sin_addr) <<  ntohs(si_client.sin_port) << endl;
-	}
-	else {
-		cout << "Unknown packet" << endl;
-	}
+    listen(sock_server, 5);
+    cout << "WAITIN!\n";
+    if(( sock_new = accept(sock_server, NULL, NULL)) < 0){
+	cout << "accept";
+	return 0;
     }
 
-    //send some!
-    while (true)
-    {
-            cin >> buf;
-
-       //now reply the client with the same data
-       if (sendto(server_socket, &buf, sizeof(buf), 0, (struct sockaddr*) &si_unknown, slen) == -1)
-       {
-            cout << "sendto()";
-	    exit(1);
-       }
+    int a;
+    bzero(buf,sizeof(buf));
+    cout << "IM HERE :)))\n";
+    while ((a = recv(sock_new, buf, sizeof(buf), 0)) > 0){
+	cout << "Buffer: " << buf << endl;
+	bzero(buf,sizeof(buf));
     }
+    cout << a << strerror(errno) << endl;
 
     return 0;
 }
+
